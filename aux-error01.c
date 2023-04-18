c106b33059789accb13b10dc54b68eafd97507e5
franklyn255 <122846507+franklyn255@users.noreply.github.com>
2023-04-18 13:31:44 +0100

    Update aux-error01.c

--- aux-error01.c
+++ aux-error01.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * strcat_cd - function that concatenates the message for cd error
+ * strcat_cd - concatenates the message for cd error
  *
  * @datash: data relevant (directory)
  * @msg: message to print
  * @error: output message
  * @ver_str: counter lines
- * Return: error message
+ * Return: error messages
  */
 char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 {
